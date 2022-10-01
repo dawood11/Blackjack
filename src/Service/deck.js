36e17ddfe75b6859585fc7b355a85e69da52fe27
Dawood Ahmad <dawood11@hotmail.com>
2022-10-01 22:22:21 +0200

    deck of cards

---
+++ src/Service/deck.js
@@ -0,0 +1,212 @@
+const deckOfCards = [
+  {
+    img: "./assets/A-Spades.png",
+    value: 11,
+  },
+  {
+    img: "./assets/A-Diamonds.png",
+    value: 11,
+  },
+  {
+    img: "./assets/A-Clubs.png",
+    value: 11,
+  },
+  {
+    img: "./assets/A-Hearts.png",
+    value: 11,
+  },
+  {
+    img: "./assets/2-Spades.png",
+    value: 2,
+  },
+  {
+    img: "./assets/2-Diamonds.png",
+    value: 2,
+  },
+  {
+    img: "./assets/2-Clubs.png",
+    value: 2,
+  },
+  {
+    img: "./assets/2-Hearts.png",
+    value: 2,
+  },
+  {
+    img: "./assets/3-Spades.png",
+    value: 3,
+  },
+  {
+    img: "./assets/3-Diamonds.png",
+    value: 3,
+  },
+  {
+    img: "./assets/3-Clubs.png",
+    value: 3,
+  },
+  {
+    img: "./assets/3-Hearts.png",
+    value: 3,
+  },
+  {
+    img: "./assets/4-Spades.png",
+    value: 4,
+  },
+  {
+    img: "./assets/4-Diamonds.png",
+    value: 4,
+  },
+  {
+    img: "./assets/4-Clubs.png",
+    value: 4,
+  },
+  {
+    img: "./assets/4-Hearts.png",
+    value: 4,
+  },
+  {
+    img: "./assets/5-Spades.png",
+    value: 5,
+  },
+  {
+    img: "./assets/5-Diamonds.png",
+    value: 5,
+  },
+  {
+    img: "./assets/5-Clubs.png",
+    value: 5,
+  },
+  {
+    img: "./assets/5-Hearts.png",
+    value: 5,
+  },
+  {
+    img: "./assets/6-Spades.png",
+    value: 6,
+  },
+  {
+    img: "./assets/6-Diamonds.png",
+    value: 6,
+  },
+  {
+    img: "./assets/6-Clubs.png",
+    value: 6,
+  },
+  {
+    img: "./assets/6-Hearts.png",
+    value: 6,
+  },
+  {
+    img: "./assets/7-Spades.png",
+    value: 7,
+  },
+  {
+    img: "./assets/7-Diamonds.png",
+    value: 7,
+  },
+  {
+    img: "./assets/7-Clubs.png",
+    value: 7,
+  },
+  {
+    img: "./assets/7-Hearts.png",
+    value: 7,
+  },
+  {
+    img: "./assets/8-Spades.png",
+    value: 8,
+  },
+  {
+    img: "./assets/8-Diamonds.png",
+    value: 8,
+  },
+  {
+    img: "./assets/8-Clubs.png",
+    value: 8,
+  },
+  {
+    img: "./assets/8-Hearts.png",
+    value: 8,
+  },
+  {
+    img: "./assets/9-Spades.png",
+    value: 9,
+  },
+  {
+    img: "./assets/9-Diamonds.png",
+    value: 9,
+  },
+  {
+    img: "./assets/9-Clubs.png",
+    value: 9,
+  },
+  {
+    img: "./assets/9-Hearts.png",
+    value: 9,
+  },
+  {
+    img: "./assets/10-Spades.png",
+    value: 10,
+  },
+  {
+    img: "./assets/10-Diamonds.png",
+    value: 10,
+  },
+  {
+    img: "./assets/10-Clubs.png",
+    value: 10,
+  },
+  {
+    img: "./assets/10-Hearts.png",
+    value: 10,
+  },
+  {
+    img: "./assets/J-Spades.png",
+    value: 10,
+  },
+  {
+    img: "./assets/J-Diamonds.png",
+    value: 10,
+  },
+  {
+    img: "./assets/J-Clubs.png",
+    value: 10,
+  },
+  {
+    img: "./assets/J-Hearts.png",
+    value: 10,
+  },
+  {
+    img: "./assets/Q-Spades.png",
+    value: 10,
+  },
+  {
+    img: "./assets/Q-Diamond.png",
+    value: 10,
+  },
+  {
+    img: "./assets/Q-Clubs.png",
+    value: 10,
+  },
+  {
+    img: "./assets/Q-Hearts.png",
+    value: 10,
+  },
+  {
+    img: "./assets/K-Spades.png",
+    value: 10,
+  },
+  {
+    img: "./assets/K-Diamonds.png",
+    value: 10,
+  },
+  {
+    img: "./assets/K-Clubs.png",
+    value: 10,
+  },
+  {
+    img: "./assets/K-Hearts.png",
+    value: 10,
+  },
+];
+
+export default deckOfCards;
