77c9a680eff793b3d7ea1ca130e6e020779ec040
Dawood Ahmad <dawood11@hotmail.com>
2022-10-03 04:18:07 +0200

    added overall score overview

--- src/Components/Card/CardSet.js
+++ src/Components/Card/CardSet.js
@@ -2,14 +2,18 @@ import './Card.css';
 
 import Card from './Card';
 import React from 'react';
+import { currentPlayerScore } from '../../Service/localStorageKeys';
 
 const CardSet = ({roleName, score, cardsInHand, msg}) => {
+  const currentOverallScore = localStorage.getItem(currentPlayerScore);
+
   return (
     <div>
-      <h2>{roleName}({score}) {msg}</h2>
+      <h2>{roleName} ({score}) {msg}</h2>
       <div className='cardContainer'>
         {cardsInHand.length > 0 && cardsInHand.map((cardItem, index) => <Card key={index} card={cardItem} />)}
       </div>
+      {currentOverallScore !== null && roleName === 'Player' && 'Overall score ' + currentOverallScore}
     </div>
   );
 };
