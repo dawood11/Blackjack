b872cff45c2b13f29e0463877dee2ff2d699bb1d
Dawood Ahmad <dawood11@hotmail.com>
2022-10-03 02:56:48 +0200

    Created highscore list.. showing top 10

--- src/Components/Highscore/Highscore.js
+++ src/Components/Highscore/Highscore.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { highScoreList } from '../../Service/localStorageKeys';
 
 const Highscore = ({ setStageModeToHomeScreen }) => {
+  const highScoreListData = JSON.parse(localStorage.getItem(highScoreList));
+
   return (
     <div>
       <h1>Highscore</h1>
+      {
+        highScoreListData === null
+          ? <p>No highscore recorded yet...</p>
+          : <React.Fragment>
+            {
+              highScoreListData
+                .filter((e, i) => (i < 10))
+                .map((scoreData, index) => <p key={index}>
+                  {scoreData.name} - {scoreData.score}
+                </p>)
+            }
+          </React.Fragment>
+      }
       <button onClick={() => setStageModeToHomeScreen()}>Home</button>
     </div>
   );
